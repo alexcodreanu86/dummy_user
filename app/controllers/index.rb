a30f9ec98e887008f426d03af8cf8a0b571a6da9
alexcodreanu86 <alexcodreanu86@yahoo.com>
2014-02-19 13:29:44 -0600

    Added views for signup and home

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,3 +2,30 @@ get '/' do
   # Look in app/views/index.erb
   erb :index
 end
+
+post '/' do
+
+  # get user credentials
+  # check, and set session
+  # if login is correct
+end
+
+get '/shorty' do
+  # check if user logged in
+  # if so, render shorty page
+  # else, redirect to sign in page
+  erb :shorty
+end
+
+get '/logout' do
+  # delete session
+  # redirect home
+end
+
+get '/signup' do
+  erb :signup
+end
+
+post '/signup' do
+  # create user
+end
